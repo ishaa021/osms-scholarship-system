2b5fc03fc6d7eb3c15b40d0ae611316d9fb3336a
ishaa021 <ahsi.21075@gmail.com>
2026-04-16 13:24:38 +0530

    Fixed port for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -31,8 +31,11 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ MongoDB connected');
-    app.listen(process.env.PORT, () =>
-      console.log(`✅ Server running on port ${process.env.PORT}`)
-    );
+
+ const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
   })
   .catch((err) => console.error('❌ DB connection error:', err));
